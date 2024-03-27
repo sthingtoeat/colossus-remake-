2fd28c2de1e46d07a25dbf28b33eab29c06dd713
unknown <1040606497@qq.com>
2024-03-27 16:42:26 +0800

    前端初具雏形

---
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -0,0 +1,16 @@
+<template>
+   <ContentField>Bot页面</ContentField>
+</template>
+<script>
+import ContentField from '../../../components/ContentField.vue'
+
+export default {
+    components:{
+        ContentField,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
